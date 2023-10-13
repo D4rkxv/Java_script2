f06fce38695c4456c0d3909214a00e2d328437ff
D4rkxv <112972404+D4rkxv@users.noreply.github.com>
2023-10-13 10:29:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+let not_funny_joke= "Nazwać programistę informatykiem, to jak nazwać lekarza znachorem."
+
+//zad 1
+alert(not_funny_joke.length)
+//zad 2 
+console.log(not_funny_joke + "nieprawdaż")
+//zad 3
+let letter = not_funny_joke.charAt(8) 
+console.log(letter)
+//zad 4
+console.log(not_funny_joke[not_funny_joke.length -3])
+//zad 5
+console.log(not_funny_joke.toUpperCase())
+//zad 6 
+not_funny_joke= "Nazwać webmastera informatykiem, to jak nazwać lekarza znachorem."
+alert(not_funny_joke)
+//zad 7 
+console.log("Hello World!")
+//zad 8 
+let Hello = "Hello World!"
+console.log(typeof Hello)
+//zad 9 
+let e = 2.718228
+alert(e)
+//zad 10
+let zmienna = 12
+console.log(typeof zmienna)
+zmienna = "Java Script"
+console.log(typeof zmienna)
+//zad 11
+let opis = "JavaScript to język programowania."
+console.log("Informacja: "+ opis)
+//zad 12
+let a = 21
+let b = 17
+alert(a+b)
+//zad 13
+let okok = prompt("podaj liczbe: ")
+alert("twoja liczba to: "+ okok)
